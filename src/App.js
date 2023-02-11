52c06c6a12a86f8d60c3999475ada3153cb8df50
serah2023 <123532441+serah2023@users.noreply.github.com>
2023-02-11 14:53:53 +0330

    improved transformed codes

--- src/App.js
+++ src/App.js
@@ -6,15 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>weather app</h1>
-        <WeatherSearch city="paris" />
-        <p>
         
-          <a href="https://github.com/serah2023/weather-app-react">
-            open source code
-          </a>
-          by sara zolfaghari;
-        </p>
+ <WeatherSearch />
       </header>
     </div>
   );
--- src/WeatherSearch.js
+++ src/WeatherSearch.js
@@ -2,59 +2,205 @@ import React, { useState } from "react";
 import axios from "axios";
 
 export default function WeatherSearch() {
-  const [city, setCity] = useState("");
-  const [loaded, setLoaded] = useState(false);
-  const [weather, setWeather] = useState({});
+  return (
+    <div className="container">
+      <div className="weather-app-wrapper">
+        <div className="weather-app">
+          <div class="mb-3 top-links">
+            <a href="" className="p-3 ">
+              lisbon
+            </a>
+            <a href="" className="p-3">
+              paris
+            </a>
 
-  function displayWeather(response) {
-    setLoaded(true);
-    setWeather({
-      temperature: response.data.main.temp,
-      wind: response.data.wind.speed,
-      humidity: response.data.main.humidity,
-      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
-      description: response.data.weather[0].description,
-    });
-  }
+            <a href="" className="p-3">
+              sydney
+            </a>
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    let apiKey = "094780c710fa4efd669f0df8c3991927";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(displayWeather);
-  }
+            <a href="" className="p-3">
+              italy
+            </a>
+          </div>
 
-  function updateCity(event) {
-    setCity(event.target.value);
-  }
+          <form className="search-form" id="search-form">
+            <div className="row">
+              <div className="col-6">
+                <input
+                  type="search"
+                  placeholder="Type a city.."
+                  autofocus="on"
+                  autocomplete="off"
+                  id="city-input"
+                  className="form-control shadow-sm mb-3 "
+                />
+              </div>
+              <div className="col-3">
+                <input
+                  type="submit"
+                  value="Search"
+                  className="form-control btn btn-primary shadow-sm w-100"
+                />
+              </div>
 
-  let form = (
-    <form onSubmit={handleSubmit}>
-      <input type="search" placeholder="Enter a city.." onChange={updateCity} />
-      <button type="Submit">Search</button>
-    </form>
-  );
+              <div className="col-3">
+                <button
+                  className="btn btn-success w-100"
+                  id="current-location-button"
+                >
+                  Current
+                </button>
+              </div>
+            </div>
+          </form>
 
-  if (loaded) {
-    return (
-      <div>
-        {form}
-        <ul>
-          <li>Temperature: {Math.round(weather.temperature)}°C</li>
-          <li>Description: {weather.description}</li>
-          <li>Humidity: {weather.humidity}%</li>
-          <li>Wind: {weather.wind}km/h</li>
-          <li>
-            <img src={weather.icon} alt={weather.description} />
-          </li>
-        </ul>
+          <div className="overview">
+            <h1 id="city"> lisbon</h1>
+            <ul>
+              <li>
+                {/* last updated: <span id="date"></span> */}
+                saturday 12:45
+              </li>
+              <li>clear</li>
+
+              {/* <li id="description">
+
+                        </li> */}
+            </ul>
+          </div>
+
+          <div className="row">
+            <div className="col-6">
+              <div className="d-flex weather-temperature">
+                <img src="" alt=" sunny" id="icon" />
+
+                <div>
+                  <strong id="temperature">19</strong>
+
+                  <span className="units">
+                    <a href="#" id="celsius-link" className="active">
+                      °C
+                    </a>{" "}
+                    |{/* <a href="#" id="Fahrenheit-link">°F</a> */}
+                  </span>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-6">
+              <ul>
+                <li>
+                  Percipitation: <span id="Percipitation"></span>
+                </li>
+
+                <li i>
+                  wind: <span id="wind"> </span> km/h
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="container mt-5">
+            <div className="row ">
+              <div className="col-2 nextdays">
+                <ul>
+                  <li> Sun</li>
+                  <br />
+
+                  <li>
+                    {" "}
+                    <span>
+                      {" "}
+                      <i className="fa-solid fa-sun "></i>{" "}
+                    </span>{" "}
+                  </li>
+                  <br />
+                  <li> 9°</li>
+                </ul>
+              </div>
+
+              <div className="col-2 nextdays">
+                <ul>
+                  <li> Mon</li>
+                  <br />
+
+                  <li>
+                    {" "}
+                    <span>
+                      {" "}
+                      <i className="fa-solid fa-sun "></i>
+                    </span>{" "}
+                  </li>
+                  <br />
+                  <li> 9°</li>
+                </ul>
+              </div>
+
+              <div className="col-2 nextdays">
+                <ul>
+                  <li>Tus</li>
+                  <br />
+
+                  <li>
+                    {" "}
+                    <span>
+                      {" "}
+                      <i className="fa-solid fa-sun "></i>
+                    </span>{" "}
+                  </li>
+                  <br />
+                  <li> 9°</li>
+                </ul>
+              </div>
+
+              <div className="col-2 nextdays">
+                <ul>
+                  <li> Wed</li>
+
+                  <br />
+                  <li>
+                    {" "}
+                    <span>
+                      {" "}
+                      <i className="fa-solid fa-sun "></i>
+                    </span>{" "}
+                  </li>
+                  <br />
+                  <li> 9°</li>
+                </ul>
+              </div>
+
+              <div className="col-2 nextdays">
+                <ul>
+                  <li> Thu</li>
+
+                  <br />
+                  <li>
+                    {" "}
+                    <span>
+                      {" "}
+                      <i className="fa-solid fa-sun "></i>
+                    </span>{" "}
+                  </li>
+                  <br />
+                  <li> 9°</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <small>
+          <a
+            href="https://github.com/wecodeschool/react-weather"
+            target="_blank"
+          >
+            Open-source code
+          </a>
+          , by <a href="#">sara zolfaghari</a>
+       
+        </small>
       </div>
-    );
-  } else {
-    return form;
-    
-  }
-
-
- 
+    </div>
+  );
 }
